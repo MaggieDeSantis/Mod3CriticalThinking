d7af2a4fb7df837e26c8fc7858abcb83f4453855
MaggieDeSantis <sheisntreal@gmail.com>
2025-09-27 21:40:30 -0400

    start of code

---
+++ Taxes.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Taxes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your weekly income: ");
+        double income = scanner.nextDouble();
+
+    }
+}
