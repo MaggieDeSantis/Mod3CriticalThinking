97c44406be55d7725c9cc397561a3180e5b02c37
MaggieDeSantis <sheisntreal@gmail.com>
2025-09-27 23:41:27 -0400

    final update.

--- Taxes.java
+++ Taxes.java
@@ -2,9 +2,9 @@
 public class Taxes {
 
     public static void main(String[] args) {
-        double income = 1200;
+        double income = 100;
 
-        double taxRate = 0.0;
+        double taxRate = 0.00;
         if (income < 500) {
             taxRate = 0.10;
         } else if (income < 1500) {
@@ -14,7 +14,8 @@ public class Taxes {
         } else if (income >= 2500) {
             taxRate = 0.30;
         }
-        System.out.println("your weekly income is: " + income);
+        System.out.println("your weekly income is: $" + income);
         System.out.println("Your tax rate is: " + taxRate);
+        System.out.println("Weekly tax withheld: $" + (income * taxRate));
     }
 }
