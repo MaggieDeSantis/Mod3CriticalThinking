c6ef774d7f57a4fd12080f88d34be63c4f36df81
MaggieDeSantis <sheisntreal@gmail.com>
2025-09-27 21:48:09 -0400

    Added tax as decimals

--- Taxes.java
+++ Taxes.java
@@ -9,5 +9,13 @@ public class Taxes {
         System.out.print("Enter your weekly income: ");
         double income = scanner.nextDouble();
 
+        double taxRate;
+        if (income < 500) {
+            taxRate = 0.10;
+        } else if (income < 1500) {
+            taxRate = 0.15;
+        } else if (income < 2500) {
+            taxRate = 0.20;
+        }
     }
 }
